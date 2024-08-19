2cf4a5b161dc41a206426e539c0848500a3fdb29
kriiuo <161237243+kriiuo@users.noreply.github.com>
2024-08-19 17:47:09 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buyButton = document.getElementById('buyButton');
-    
-    buyButton.addEventListener('click', () => {
-        alert('Thank you for your purchase! Your pen will be shipped soon.');
-    });
-});
